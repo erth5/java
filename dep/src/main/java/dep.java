659161971aaef7a3f54e2332911723d370520b2a
geflltername <rthem@gmx.de>
2019-11-13 21:53:26 +0100

    depJSON

--- dep/src/main/java/dep.java
+++ dep/src/main/java/dep.java
@@ -1,8 +1,13 @@
-import org.json.JSONString;
+import org.json.JSONStringer;
 
 public class dep {
     public static void main(String[] args){
-
+        String mystr = new JSONStringer()
+                .object()
+                .key("JSON")
+                .value("hello")
+                .endObject()
+                .toString();
         System.out.println("finish");
     }
 }
