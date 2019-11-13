738f88d7c3ccfa7180400c17aa0d3cb9d4ad7054
geflltername <rthem@gmx.de>
2019-11-13 23:28:51 +0100

    dep Microsoft Excel

--- dep/src/main/java/dep.java
+++ dep/src/main/java/dep.java
@@ -1,13 +1,22 @@
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.json.JSONStringer;
 
 public class dep {
     public static void main(String[] args){
+        //JSON
         String mystr = new JSONStringer()
                 .object()
                 .key("JSON")
                 .value("hello")
                 .endObject()
                 .toString();
+
+        //Microsoft Excel
+        HSSFWorkbook workbook = new HSSFWorkbook();
+
+
+
+
         System.out.println("finish");
     }
 }
