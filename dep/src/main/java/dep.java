e59d24d44eec94ed311d8f089f378f63401f1153
geflltername <rthem@gmx.de>
2019-11-22 22:45:41 +0100

    dep update

--- dep/src/main/java/dep.java
+++ dep/src/main/java/dep.java
@@ -7,28 +7,21 @@ public class dep {
     public static void main(String[] args){
 
         //JSON
-        //JSON API using 1
+        //using 1
         String mystr = new JSONStringer()
                 .object()
                 .key("JSON")
                 .value("hello")
                 .endObject()
                 .toString();
-
-        //JSON API using 2
+        //using 2
         JSONObject obj = new JSONObject("{ \"hello\" : \"world\" }");
         String x = obj.toString();
         obj.put("hello", "again");
         System.out.println("finish" + x + obj);
-
+        //using 3
         JSONArray jsonArray = new JSONArray("[naffer]");
         System.out.println(jsonArray.toString());
-
-        //JSON Api simple framework#
-        String neww = "testfile.json";
-        ////Object testfile = new Object();
-        ////org.json.simple.JSONObject sob = new org.json.simple.JSONObject(testfile.json);
-        //System.out.println(sob);
 
         //Microsoft Excel
         HSSFWorkbook workbook = new HSSFWorkbook();
