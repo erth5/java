ba8bbf1a7e2217c199cbb688cef0d1470446bad5
geflltername <rthem@gmx.de>
2019-11-23 13:20:17 +0100

    dep update

--- dep/src/main/java/testpars.java
+++ dep/src/main/java/testpars.java
@@ -1,6 +1,12 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class testpars {
 
     public static void main(String[] args) {
@@ -18,8 +24,11 @@ public class testpars {
         String x1 = jo.getString("firstName");
         String x2 = jo.getString("lastName");
 
-        System.out.println(x1);
-        System.out.println(x2);
+        //System.out.println(x1);
+        //System.out.println(x2);
+
+        JSONArray y = readfromfile("file.json");
+        System.out.print(y);
         //String x = ja.getJSONObject(0).toString();
 
         //tests
@@ -31,5 +40,17 @@ public class testpars {
 
         //JSONObject mainObj = new JSONObject();
         //mainObj.put("employees", ja);
+    }
+    public static void writetofile(JSONArray x){
+        Path file = Paths.get("output.json");
+        try (BufferedWriter writer = Files.newBufferedWriter(file)){
+            writer.write(x.toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static JSONArray readfromfile(String path){
+
     }
 }
