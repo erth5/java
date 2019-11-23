f89c14ad588013ed7e6f8dc013fdaad214215c97
geflltername <rthem@gmx.de>
2019-11-23 19:03:11 +0100

    dep update

---
+++ dep/src/main/java/parser.java
@@ -0,0 +1,82 @@
+import org.jetbrains.annotations.NotNull;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.*;
+import java.util.Iterator;
+
+public class parser {
+    public static void main(String[] args) throws IOException {
+        test_calender();
+    }
+    //*from DB
+    //++to android
+    public static void test_calender() throws IOException {
+        JSONArray calender = new JSONArray();
+        JSONObject event;
+
+        for (int events = 4; events >= 0;events--){
+            event = new JSONObject();
+            event.put("hallo", "bey");
+            //System.out.println(event);
+            //out_obj(event, "calendar.json");
+        }
+        in_arr("calendar.json", "calendar");
+    }
+    //++to win
+    //*from win
+    //*from android
+
+    //*general
+    //out
+    private static void out_obj(JSONObject obj, String file){
+        try (FileWriter writer = new FileWriter(file)){
+            writer.write(obj.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void out_arr(JSONArray obj, String file){
+        try (FileWriter writer = new FileWriter(file)){
+            writer.write(obj.toJSONString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //in
+    //parse a array of objects
+    public static void in_arr(String file, String type) throws IOException {
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader(file)){
+            JSONObject ev = (JSONObject) parser.parse(reader);
+            switch (type){
+                case "calendar"://when it`s a calendar file
+                    JSONArray arr = (JSONArray) ev.get("event");
+                    for (Object o : arr) {
+                        System.out.println(o);
+                    }
+                    break;
+                case "customer":
+                    //TODO
+                    break;
+                default:
+                    break;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    //parse only one object
+    public static void in_obj(String file) throws IOException {
+        JSONParser parser = new JSONParser();
+        try (Reader reader = new FileReader(file)){
+            JSONObject obj = (JSONObject) parser.parse(reader);
+            System.out.println(obj);
+            } catch (ParseException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
--- dep/src/main/java/testpars.java
+++ dep/src/main/java/testpars.java
@@ -1,8 +1,11 @@
 import org.json.JSONArray;
 import org.json.JSONObject;
+import org.json.JSONWriter;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -23,12 +26,11 @@ public class testpars {
         //to DB
         String x1 = jo.getString("firstName");
         String x2 = jo.getString("lastName");
-
         //System.out.println(x1);
         //System.out.println(x2);
 
-        JSONArray y = readfromfile("file.json");
-        System.out.print(y);
+        //String y = readfromfile("file.json");
+        //System.out.print(y);
         //String x = ja.getJSONObject(0).toString();
 
         //tests
@@ -40,17 +42,5 @@ public class testpars {
 
         //JSONObject mainObj = new JSONObject();
         //mainObj.put("employees", ja);
-    }
-    public static void writetofile(JSONArray x){
-        Path file = Paths.get("output.json");
-        try (BufferedWriter writer = Files.newBufferedWriter(file)){
-            writer.write(x.toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    public static JSONArray readfromfile(String path){
-
     }
 }
