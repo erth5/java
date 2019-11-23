c5d6cc667419185db65cac8c3d8e4cf36d4d8b5c
geflltername <rthem@gmx.de>
2019-11-23 21:02:07 +0100

    update

--- dep/src/main/java/parser.java
+++ dep/src/main/java/parser.java
@@ -3,10 +3,7 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
 
 public class parser {
     public static void main(String[] args) throws IOException {
@@ -16,15 +13,25 @@ public class parser {
     //++to android
     public static void test_calender() throws IOException {
         JSONArray calender = new JSONArray();
-        JSONObject event;
 
-        for (int events = 4; events >= 0;events--){
-            event = new JSONObject();
-            event.put("hallo", "bey");
-            //System.out.println(event);
-            //out_obj(event, "calendar.json");
-        }
-        in_arr("calendar.json", "calendar");
+
+        //examples
+        JSONObject ex = new JSONObject();
+        ex.put("hello", "world");
+        ex.put("second", "line");
+        JSONObject xe = new JSONObject();
+        xe.put("l", "h");
+        xe.put("j", "d");
+        JSONArray example = new JSONArray();
+        example.add(ex);
+        example.add(xe);
+        example.add(1, "df");
+        example.add(0, "TT");
+
+        //testing
+        in_cal("calendar.json", "calendar");
+        out_obj(ex, "whateverobj.json");
+        out_arr(example, "whateverarr.json");
     }
     //++to win
     //*from win
@@ -39,6 +46,8 @@ public class parser {
             e.printStackTrace();
         }
     }
+
+    //in case of json start with a array
     public static void out_arr(JSONArray obj, String file){
         try (FileWriter writer = new FileWriter(file)){
             writer.write(obj.toJSONString());
@@ -48,7 +57,7 @@ public class parser {
     }
     //in
     //parse a array of objects
-    public static void in_arr(String file, String type) throws IOException {
+    public static void in_cal(String file, String type) throws IOException {
         JSONParser parser = new JSONParser();
         try (Reader reader = new FileReader(file)){
             JSONObject ev = (JSONObject) parser.parse(reader);
@@ -69,6 +78,19 @@ public class parser {
             e.printStackTrace();
         }
     }
+
+    //parse a array
+    public static void in_arr(String file) throws FileNotFoundException {
+        JSONParser parser = new JSONParser();
+        try (Reader r = new FileReader(file)){
+            JSONArray arr = new JSONArray();
+            parser.parse(r);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     //parse only one object
     public static void in_obj(String file) throws IOException {
