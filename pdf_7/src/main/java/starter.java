fd376843a60e7415af8a521fffa5e9a2a1f08752
geflltername <rthem@gmx.de>
2019-12-03 20:19:42 +0100

    up

--- pdf_7/src/main/java/starter.java
+++ pdf_7/src/main/java/starter.java
@@ -3,6 +3,6 @@ import java.net.MalformedURLException;
 
 public class starter {
     public static void main(String[] args) throws FileNotFoundException, MalformedURLException {
-        pdf.create_pdf("file.pdf");
+        //pdf.create_pdf("file.pdf");
     }
 }
